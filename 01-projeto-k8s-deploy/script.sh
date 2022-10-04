b9d942c01996f95d528a4972044d1774031210f9
root <root@Brenno.localdomain>
2022-10-03 21:12:34 -0300

    commit v2

--- 01-projeto-k8s-deploy/script.sh
+++ 01-projeto-k8s-deploy/script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Criando as imagens...."
 
 docker build -t brennocm/projeto-backend:1.0 backend/.
@@ -7,3 +9,11 @@ echo "Realizando push das imagens para o Docker Hub"
 
 docker push brennocm/projeto-backend:1.0
 docker push brennocm/projeto-database:1.0
+
+echo "Criando serviços no Cluster K8s"
+
+kubectl apply -f ../services.yml
+
+echo "Criando os deployments...."
+
+kubectl apply -f ../deployment.yml
